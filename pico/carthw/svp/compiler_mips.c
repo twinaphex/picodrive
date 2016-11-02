fa90018b896e57ca53f69127264ad910bd2ec557
robson-alcantara <robson.poli@gmail.com>
2016-11-02 11:12:02 -0300

    removing debug code

--- pico/carthw/svp/compiler_mips.c
+++ pico/carthw/svp/compiler_mips.c
@@ -60,20 +60,8 @@ static int get_inc(int mode)
 	return inc;
 }
 
-static int ssp_pm_read_count = 0;
-
 u32 ssp_pm_read(int reg)
 {
-	ssp_pm_read_count++;
-
-	if( ssp_pm_read_count == 64 ) {
-		ssp_pm_read_count += 0;
-	}
-
-	if( ssp_pm_read_count == 65 ) {
-		ssp_pm_read_count += 0;
-	}
-
 	u32 d = 0, mode;
 
 	if (ssp->emu_status & SSP_PMC_SET)
@@ -114,14 +102,10 @@ u32 ssp_pm_read(int reg)
 	if (d & 0x000f) { dst &= ~0x000f; dst |= d & 0x000f; } \
 }
 
-static int ssp_pm_write_count = 0;
-
 void ssp_pm_write(u32 d, int reg)
 {
 	unsigned short *dram;
 	int mode, addr;
-
-	ssp_pm_write_count++;
 
 //	unsigned int x;
 //	asm volatile ("move %0, $t7" : "=r" (x));
@@ -250,141 +234,6 @@ static struct
 #define A_COND_LT 0xb
 #define A_COND_GT 0xc
 #define A_COND_LE 0xd
-
-static int count_ = 0;
-static int breakpoint_count = 0;
-unsigned int a0_;
-unsigned int s5_;
-int *mem_value = 0x904cfd0;
-int *mem_value2 = 0x904cef0;
-int prev_mem_value = 0;
-
-void breakpoint(void) {
-
-//	asm volatile ("addiu $sp, $sp, -60");
-//	asm volatile ("sw $s7, 56($sp)");
-//	asm volatile ("sw $s6, 52($sp)");
-//	asm volatile ("sw $s5, 48($sp)");
-//	asm volatile ("sw $s4, 44($sp)");
-//	asm volatile ("sw $s3, 40($sp)");
-//	asm volatile ("sw $s2, 36($sp)");
-//	asm volatile ("sw $s1, 32($sp)");
-//	asm volatile ("sw $t9, 28($sp)");
-//	asm volatile ("sw $t8, 24($sp)");
-//	asm volatile ("sw $t7, 20($sp)");
-//	asm volatile ("sw $t6, 16($sp)");
-//	asm volatile ("sw $t5, 12($sp)");
-//	asm volatile ("sw $t4, 8($sp)");
-//	asm volatile ("sw $a3, 4($sp)");
-//	asm volatile ("sw $a2, ($sp)");
-
-//	if (ssp_pm_read_count == 64) {
-//		breakpoint_count++;
-//		if (breakpoint_count == 18)  {
-//
-//			asm volatile ("move %0, $a0" : "=r" (a0_));
-//			asm volatile ("sw $a0, %0" : "=m" (a0_));
-//
-//			asm volatile ("move %0, $s5" : "=r" (s5_));
-//			asm volatile ("sw $s5, %0" : "=m" (s5_));
-//
-//			//printf("1:$t7 -> %04x\n", x);
-//
-//			count_ += 0;
-//		}
-//	}
-
-	asm volatile ("move %0, $a0" : "=r" (a0_));
-	asm volatile ("sw $a0, %0" : "=m" (a0_));
-
-	//printf("%d: $a0: %d\n", breakpoint_count, a0_);
-	//printf("$a0: %d; ", a0_);
-
-	if( ( breakpoint_count >= 663674  ) ) { // && ( breakpoint_count < 333430 ) ) {   489982
-		//printf("%d: $a0: %d\n", breakpoint_count, a0_);
-
-		if( breakpoint_count == 5109 ) {
-			count_ += 0;
-		}
-//
-//		if( breakpoint_count == 5136 ) {
-//			count_ += 0;
-//		}
-	}
-
-	if ( ( count_ >= 6000 ) ) {// && ( count < 650 ) ){
-		//printf("$a0: %d; ", a0_);
-		printf("%d: $a0: %d; ", breakpoint_count, a0_);
-		if( *mem_value != prev_mem_value ) {
-			//printf("%d: $a0: %d\n", breakpoint_count, a0_);
-			//printf("mem adr: 0x%08x d: 0x%08x mem adr2: 0x%08x d: 0x%08x count: %d\n", mem_value, *mem_value, mem_value2, *mem_value2, count_);
-
-			if ( ( *mem_value == 0x00240100 ) ) {// && ( count < 650 ) ){
-				count_ += 0;
-			}
-//
-//			else {
-//				count_ += 0;
-//			}
-		}
-		prev_mem_value = *mem_value;
-
-		if( ( a0_ == 768 ) ) { // && ( count_ >= 2000 ) ) {
-			count_ += 0;
-		}
-	}
-
-//	if( ( a0_ == 10121) && ( count_ >= 2000 ) ) {
-//		count_ += 0;
-//	}
-//
-//	if( ( a0_ == 282) && ( count_ >= 4040 ) ) {
-//		count_ += 0;
-//	}
-//
-//	if( ( a0_ == 2068) && ( count_ >= 4037 ) ) {
-//		count_ += 0;
-//	}
-
-	breakpoint_count++;
-
-//	asm volatile ("sw $a2, ($sp)");
-//	asm volatile ("sw $a3, 4($sp)");
-//	asm volatile ("sw $t4, 8($sp)");
-//	asm volatile ("sw $t5, 12($sp)");
-//	asm volatile ("sw $t6, 16($sp)");
-//	asm volatile ("sw $t7, 20($sp)");
-//	asm volatile ("sw $t8, 24($sp)");
-//	asm volatile ("sw $t9, 28($sp)");
-//	asm volatile ("sw $s1, 32($sp)");
-//	asm volatile ("sw $s2, 36($sp)");
-//	asm volatile ("sw $s3, 40($sp)");
-//	asm volatile ("sw $s4, 44($sp)");
-//	asm volatile ("sw $s5, 48($sp)");
-//	asm volatile ("sw $s6, 52($sp)");
-//	asm volatile ("sw $s7, 56($sp)");
-//	asm volatile ("addiu $sp, $sp, -60");
-}
-
-static int first_time = 1;
-unsigned int a1_;
-unsigned int a2_;
-
-void breakpoint2(void) {
-	if (ssp_pm_read_count == 64) {
-		if (breakpoint_count == 18)  {
-			if (first_time) {
-				asm volatile ("move %0, $a1" : "=r" (a1_));
-				asm volatile ("sw $a1, %0" : "=m" (a1_));
-
-				asm volatile ("move %0, $a2" : "=r" (a2_));
-				asm volatile ("sw $a2, %0" : "=m" (a2_));
-
-				first_time = 0;
-			}
-		}
-	}
-}
 
 void update_mips_flags_register(void) {
 	MIPS_BLEZ(MIPS_s5,4);
@@ -2239,15 +2088,6 @@ static int translate_op(unsigned int op, int *pc, int imm, int *end_cond, int *j
 			if (tmpv != A_COND_AL)
 				tr_mov16_cond(tr_neg_cond(tmpv), 0, *pc);
 			tr_r0_to_PC(tmpv == A_COND_AL ? imm : -1);
-
-//			MIPS_ADDIU(MIPS_sp, MIPS_sp, -72);
-//			MIPS_SW(MIPS_a1, 4,MIPS_sp);
-//			MIPS_SW(MIPS_a0, 0,MIPS_sp);
-//			emith_call(breakpoint);
-//			MIPS_LW(MIPS_a0, 0,MIPS_sp);
-//			MIPS_LW(MIPS_a1, 4,MIPS_sp);
-//			MIPS_ADDIU(MIPS_sp, MIPS_sp, 72);
-
 			ret |= 0x10000;
 			*end_cond = tmpv;
 			*jump_pc = imm;
@@ -2282,15 +2122,6 @@ static int translate_op(unsigned int op, int *pc, int imm, int *end_cond, int *j
 			if (tmpv != A_COND_AL)
 				tr_mov16_cond(tr_neg_cond(tmpv), 0, *pc);
 			tr_r0_to_PC(tmpv == A_COND_AL ? imm : -1);
-
-//			MIPS_ADDIU(MIPS_sp, MIPS_sp, -72);
-//			MIPS_SW(MIPS_a1, 4,MIPS_sp);
-//			MIPS_SW(MIPS_a0, 0,MIPS_sp);
-//			emith_call(breakpoint);
-//			MIPS_LW(MIPS_a0, 0,MIPS_sp);
-//			MIPS_LW(MIPS_a1, 4,MIPS_sp);
-//			MIPS_ADDIU(MIPS_sp, MIPS_sp, 72);
-
 			ret |= 0x10000;
 			*end_cond = tmpv;
 			*jump_pc = imm;
@@ -2468,51 +2299,6 @@ static int translate_op(unsigned int op, int *pc, int imm, int *end_cond, int *j
 					update_mips_flags_register();
 					//MIPS_SUBU(MIPS_s5,MIPS_s6,MIPS_a0);  // sub $s5, $s6, $a0, replace arm cmp
 				}
-
-//				MIPS_MOVE(MIPS_t2,MIPS_s5);
-//				if(tmpv2 == __SP_ADDU) {
-//					MIPS_LUI(MIPS_t0, 0xf000);
-//					MIPS_AND(MIPS_t1,MIPS_t3,MIPS_t0);
-//					MIPS_BEQZ(MIPS_t1,22);
-//					MIPS_NOP();
-//					MIPS_SUBU(MIPS_t1,MIPS_t0,MIPS_t1);
-//					MIPS_BEQZ(MIPS_t1,19);
-//					MIPS_NOP();
-//					MIPS_AND(MIPS_t1,MIPS_s6,MIPS_t0);
-//					MIPS_BEQZ(MIPS_t1,16);
-//					MIPS_NOP();
-//					MIPS_SUBU(MIPS_t1,MIPS_t0,MIPS_t1);
-//					MIPS_BEQZ(MIPS_t1,13);
-//					MIPS_NOP();
-//					MIPS_ADDIU(MIPS_sp, MIPS_sp, -12);
-//					MIPS_SW(MIPS_ra, 8,MIPS_sp);
-//					MIPS_SW(MIPS_a1, 4,MIPS_sp);
-//					MIPS_SW(MIPS_a0, 0,MIPS_sp);
-//					emith_call(breakpoint);
-//					MIPS_LW(MIPS_a0, 0,MIPS_sp);
-//					MIPS_LW(MIPS_a1, 4,MIPS_sp);
-//					MIPS_LW(MIPS_ra, 8,MIPS_sp);
-//					MIPS_ADDIU(MIPS_sp, MIPS_sp, 12);
-//					MIPS_SRL(MIPS_t0,MIPS_s6,16);
-//					MIPS_SRL(MIPS_t1,MIPS_t3,16);
-//					MIPS_ADDU(MIPS_s5,MIPS_t0,MIPS_t1);
-//				}
-//				MIPS_MOVE(MIPS_s6,MIPS_s5);        //TODO: try to fix negative overflow
-//				MIPS_SRL(MIPS_t0,MIPS_s5,31);
-//				MIPS_BEQZ(MIPS_t0,11);
-//				MIPS_NOP();
-//				MIPS_ADDIU(MIPS_sp, MIPS_sp, -12);
-//				MIPS_SW(MIPS_ra, 8,MIPS_sp);
-//				MIPS_SW(MIPS_a1, 4,MIPS_sp);
-//				MIPS_SW(MIPS_a0, 0,MIPS_sp);
-//				emith_call(breakpoint);
-//				MIPS_LW(MIPS_a0, 0,MIPS_sp);
-//				MIPS_LW(MIPS_a1, 4,MIPS_sp);
-//				MIPS_LW(MIPS_ra, 8,MIPS_sp);
-//				MIPS_ADDIU(MIPS_sp, MIPS_sp, 12);
-//				MIPS_SRL(MIPS_s5,MIPS_s5,1);
-				//update_mips_flags_register_fix_neg_over();
-				//MIPS_MOVE(MIPS_s5,MIPS_t2);
 #if 0
 				EOP_C_DOP_REG_XIMM(A_COND_AL,tmpv2,1,5,tmpv3,16,A_AM1_LSL, 0); // OPs r5, r5, r0, lsl #16
 #endif
@@ -2908,44 +2694,12 @@ static void *emit_block_epilogue(int cycles, int cond, int pc, int end_pc)
 	return end_ptr;
 }
 
-//int *mem_value = 0x904cfd0;
-//int prev_mem_value = 0;
-int reg_value;
-
 void *ssp_translate_block(int pc)
 {
 	unsigned int op, op1, imm, ccount = 0;
 	unsigned int *block_start, *block_end;
 	int ret, end_cond = A_COND_AL, jump_pc = -1;
 
-	int cycles;
-
-	asm volatile ("move %0, $t8" : "=r" (reg_value));
-	asm volatile ("sw $t8, %0" : "=m" (reg_value));
-
-	if ( ( count_ >= 4020 ) ) {// && ( count < 650 ) ){
-		if( *mem_value != prev_mem_value ) {
-			printf("mem adr: 0x%08x d: 0x%08x\n", mem_value, *mem_value);
-		}
-		prev_mem_value = *mem_value;
-	}
-
-//	if ( ( count_ == 569 ) ) {// && ( count < 650 ) ){
-//		if( reg_value != prev_mem_value ) {
-//			printf("reg: $t8 d: 0x%08x\n", reg_value);
-//		}
-//		prev_mem_value = reg_value;
-//	}
-
-	//printf("translate %04x -> %04x\n", pc<<1, (tcache_ptr-tcache)<<2);
-
-	if( count_ == 530 ) {
-		asm volatile ("subu $s2, $s2, 8");
-	}
-
-	if( count_ == 542 ) {
-		asm volatile ("subu $s2, $s2, 4");
-	}
 
 	block_start = tcache_ptr;
 	known_regb = 0;
@@ -2960,23 +2714,6 @@ void *ssp_translate_block(int pc)
 		op = PROGRAM(pc++);
 		op1 = op >> 9;
 		imm = (u32)-1;
-
-		if( ( ( n_in_ops + 1 ) % 500 ) == 0 ) {
-			count_ += 0;
-		}
-
-		if( ( n_in_ops + 1 ) >= 3303 ) {
-			count_ += 0;
-		}
-
-		if( pc == 769 ) {
-			count_ += 0;
-		}
-
-		asm volatile ("move %0, $s2" : "=r" (cycles));
-		asm volatile ("sw $s2, %0" : "=m" (cycles));
-
-		printf("PC: %d, op: 0x%04x, op1: 0x%04x, count: %d, i: %d, cycles: %d known_regs.r[2]: %d\n", pc, op, op>>9, count_, n_in_ops + 1, cycles, known_regs.r[2] );
 
 		if ((op1 & 0xf) == 4 || (op1 & 0xf) == 6)
 			imm = PROGRAM(pc++); // immediate
@@ -3037,36 +2774,6 @@ void *ssp_translate_block(int pc)
 #ifdef PSP
 	cache_flush_d_inval_i(block_start, block_end);
 #endif
-
-	if( pc == ( 281 + 1 ) ) {
-		count_ += 0;
-	}
-
-	if( pc == 281 ) {
-		count_ += 0;
-	}
-
-	if( count_ >= 614 ) {
-		count_ += 0;
-	}
-
-	if( ( n_in_ops + 1 ) >= 1229) {
-		count_ += 0;
-	}
-
-	if( ( n_in_ops + 1 ) >= 1323) {
-		count_ += 0;
-	}
-
-	if( ( n_in_ops + 1 ) >= 767) {
-		count_ += 0;
-	}
-
-	if( ( n_in_ops + 1 ) >= 2812) {
-		count_ += 0;
-	}
-
-	//printf("block_start -> %04x\n", block_start);
 	return block_start;
 }
 
@@ -3112,12 +2819,12 @@ int ssp1601_dyn_startup(void)
 	// hle'd blocks
 	ssp_block_table[0x800/2] = (void *) ssp_hle_800;
 	ssp_block_table[0x902/2] = (void *) ssp_hle_902;
-//	ssp_block_table_iram[ 7 * SSP_BLOCKTAB_IRAM_ONE + 0x030/2] = (void *) ssp_hle_07_030;
-//	ssp_block_table_iram[ 7 * SSP_BLOCKTAB_IRAM_ONE + 0x036/2] = (void *) ssp_hle_07_036;
-//	ssp_block_table_iram[ 7 * SSP_BLOCKTAB_IRAM_ONE + 0x6d6/2] = (void *) ssp_hle_07_6d6;
-//	ssp_block_table_iram[11 * SSP_BLOCKTAB_IRAM_ONE + 0x12c/2] = (void *) ssp_hle_11_12c;
-//	ssp_block_table_iram[11 * SSP_BLOCKTAB_IRAM_ONE + 0x384/2] = (void *) ssp_hle_11_384;
-//	ssp_block_table_iram[11 * SSP_BLOCKTAB_IRAM_ONE + 0x38a/2] = (void *) ssp_hle_11_38a;
+	ssp_block_table_iram[ 7 * SSP_BLOCKTAB_IRAM_ONE + 0x030/2] = (void *) ssp_hle_07_030;
+	ssp_block_table_iram[ 7 * SSP_BLOCKTAB_IRAM_ONE + 0x036/2] = (void *) ssp_hle_07_036;
+	ssp_block_table_iram[ 7 * SSP_BLOCKTAB_IRAM_ONE + 0x6d6/2] = (void *) ssp_hle_07_6d6;
+	ssp_block_table_iram[11 * SSP_BLOCKTAB_IRAM_ONE + 0x12c/2] = (void *) ssp_hle_11_12c;
+	ssp_block_table_iram[11 * SSP_BLOCKTAB_IRAM_ONE + 0x384/2] = (void *) ssp_hle_11_384;
+	ssp_block_table_iram[11 * SSP_BLOCKTAB_IRAM_ONE + 0x38a/2] = (void *) ssp_hle_11_38a;
 
 #endif
 
@@ -3148,10 +2855,6 @@ void ssp1601_dyn_run(int cycles)
 	int val2;
 	int val = ssp->emu_status;
 
-	if(count_== 569) {
-		val2 = 0;
-	}
-
 	if (ssp->emu_status & SSP_WAIT_MASK) return;
 
 #ifdef DUMP_BLOCK
@@ -3164,14 +2867,5 @@ void ssp1601_dyn_run(int cycles)
 	int ssp_x = ssp->gr[SSP_X].v;
 	int ssp_y = ssp->gr[SSP_Y].v;
 	int ssp_a = ssp->gr[SSP_A].v;
-
-	if(count_== 886) {
-		val2 = 0;
-	}
-
-//	if(count>=530) {
-//		ssp->emu_status = val;    // TODO: trick, FIXME
-//	}
-
 #endif
 }
